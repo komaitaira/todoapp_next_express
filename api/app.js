75b66e9f5622e468f6fbec53d1a707a1191be85e
komaitaira <komaitaira@gmail.com>
2021-05-04 15:12:01 +0900

    9999番ポートへのアクセス確認

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 
+
 // todoの配列オブジェクトを作成
 const todos = [
   { id: 1, title: "ネーム", completed: false },
@@ -9,7 +10,14 @@ const todos = [
 ];
 
 // Todo一覧の取得
-app.get('/api/todos', (req, res) => res.json(todos));
+app.get('/api/todos', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  if (!req.query.completed) {
+    return res.json(todos);
+  }
+  const completed = req.query.completed === 'true';
+  res.json(todos.filter(todo => todo.completed === completed));
+});
 
 // 9999番ポートでアクセス可能なサーバーを起動する
 app.listen(9999);
---
+++ frontend/components/todos.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+
+const pages = {
+  index: { title: "全てのTodo", fetchQuery: '' },
+  active: { title: "未完了のTodo", fetchQuery: '?completed=false' },
+  completed: { title: "完了したTodo", fetchQuery: '?completed=true' }
+};
+
+// CSRでページを切り替えるためのリンク
+const pageLinks = Object.keys(pages).map((page, index) => 
+  <Link href={`/${page === 'index' ? '' : page}`} key={index}>
+    <a style={{ marginRight: 10 }}>{pages[page].title}</a>
+  </Link>
+);
+
+export default function Todos(props) {
+  const { title, fetchQuery } = pages[props.page];
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:9999/api/todos${fetchQuery}`)
+      .then(async res => res.ok
+        ? setTodos(await res.json())
+        : alert(await res.text())
+      )
+  }, [props.page]);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{ title }</h1>
+      <ul>
+        {todos.map(({ id, title, completed }) => 
+          <li key={id}>
+            <span style={completed ? { textDecoration: 'line-through' } : {}}>
+              {title}
+            </span>
+          </li>
+        )}
+      </ul>
+      <div>{pageLinks}</div>
+    </React.Fragment>
+  )
+}
